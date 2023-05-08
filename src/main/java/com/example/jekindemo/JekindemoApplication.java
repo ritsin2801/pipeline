50840192d368dadf9ba37c43550787f0012bb26d
riteshsinha <ritesh.sinha@nagarro.com>
2023-05-08 12:25:54 +0530

    modified changs

---
+++ src/main/java/com/example/jekindemo/JekindemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jekindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JekindemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JekindemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jekindemo/JekindemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jekindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JekindemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
